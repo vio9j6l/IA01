c1f3f5e24ee382f54a31c7443ce0bea60af51843
JJ <jiajia.liu951ss@gmail.com>
2020-08-31 08:50:22 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Name: JiaJia Liu
+ * Email: JPL5840@psu.edu
+ * Course: IST 261
+ * Assignment: IA01
+ */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
