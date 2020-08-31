a89cd271175592ba4b20961afaf385cc677331fd
JJ <jiajia.liu951ss@gmail.com>
2020-08-31 09:22:36 -0400

    Use random.nextInt(5) + 1 to get integers between 1 and 5 use 2 * (random.next(5) + 1) to get integers between 2 and 10 which doubles integers between 1 and 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,28 @@
  */
 
 package com.company;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Name: JiaJia Liu");
+        System.out.println("Email: JPL5840@psu.edu");
+        System.out.println("Course: IST 261");
+        System.out.println("Assignment: IA01");
+
+        /*
+         * We multiple 2 to random.nextInt(5) + 1 which double the integers
+         * between 1 and 5 and returns integers between 2 and 10.
+         *
+         * Random.nextInt(5) methods returns a integer between 0 and 4
+         * 0 is inclusive and 4 is exclusive. To get integer between 1 and 5,
+         * we add 1 to the random.nextInt(5) method.
+         *
+         */
+        Random random = new Random();
+        int randomInt = 2 * (random.nextInt(5) + 1);
+        System.out.println(randomInt);
+
     }
 }
